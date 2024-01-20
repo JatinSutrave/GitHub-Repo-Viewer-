49d93b7d4d12f2ea7300991e216a3fc87050e5e1
Jatin Sutrave <114910576+JatinSutrave@users.noreply.github.com>
2024-01-20 22:05:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ async function fetchRepositories(username, page = 1, perPage = 10) {
     return await response.json();
 }
 
-async function fetchAndDisplay(page, username = 'jatin') {
+async function fetchAndDisplay(page, username = 'johnpapa') {
     document.getElementById('repositories').innerHTML = '<div class="text-center">Loading...</div>';
     const perPage = 10;
     const repositories = await fetchRepositories(username, page, perPage);
